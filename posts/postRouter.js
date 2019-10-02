456c853b0eaf77883e6a1d8a254c2acacd1d2461
Bobby Liberti <BobbyLiberti@Mes-iMac.local>
2019-10-02 19:27:53 -0400

    added functionality

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,78 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+
+const posts = require('./postDb');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    posts.get()
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Error getting posts"
+        })
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+    const id = req.params.id;
+    posts.getById(id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Error invaild post ID"
+        })
+    })
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId,  (req, res) => {
+    const id = req.params.id;
+    posts.remove(id)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Error deleting post"
+        })
+    })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    posts.update(id, changes)
+    .then(post => {
+        res.status(200).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Error updating post"
+        })
+    })
 
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    const id = req.params.id;
+    if(id){
+        next();
+    } else {
+        res.status(400).json({
+            message: "NOT a valid user post ID"
+        })
+    }
 };
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,38 +1,110 @@
-const express = 'express';
+const express = require('express');
+const users = require('./userDb');
+const posts = require('../posts/postDb')
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-
+    users.insert()
+    .then(post => {
+        res.status(201).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "There was an error creating the post"
+        })
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', validateUserId, (req, res) => {
+    const id = req.params.id;
+    users.getById(id)
+    .then(post => {
+        res.status(201).json(post)
+        posts.insert(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "NOT a valid user ID"
+        })
+    })
 });
 
 router.get('/', (req, res) => {
-
+    users.get()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({
+            errorMessage: "Error retrieving users."
+        })
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
+    users.getById(id)
+    .then(user => {
+        res.status(200).json(user);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Error getting post"
+        })
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', validateUserId, (req, res) => {
+    const id = req.params.id;
+    users.getUserPosts(id)
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Error getting posts"
+        })
+    })
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
+    users.remove(id)
+    .then(user => {
+        res.status(200).json({
+            message: "User has been removed"
+        })
+    })
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    users.update(id, changes)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "User could NOT be updated"
+        })
+    })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+    const id = req.params.id;
+    if(id){
+        next();
+    }
 
 };
 
